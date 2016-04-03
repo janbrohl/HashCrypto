bff8e371dbf96bd1669f5f2818d42278aea57ce8
Jan Brohl <janbrohl@t-online.de>
2016-04-03 17:33:35 +0200

    typos

--- test_hashcrypto.py
+++ test_hashcrypto.py
@@ -9,5 +9,8 @@ class TestUtilFunctions(unittest.TestCase):
 
     def test_b_xor(self):
         b = bytes(range(32))
-        unittest.assertEqual(bytes(32), hashcrypto.b_xor(b, b))
-        unittest.assertEqual(b, hashcrypto.b_xor(b, bytes(32)))
+        self.assertEqual(bytes(32), hashcrypto.b_xor(b, b))
+        self.assertEqual(b, hashcrypto.b_xor(b, bytes(32)))
+
+if __name__ == '__main__':
+    unittest.main()
