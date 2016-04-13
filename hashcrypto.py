ec86ec6f5615603507f55db3fcbfdd434b13a8da
Jan Brohl <janbrohl@t-online.de>
2016-04-13 17:22:24 +0200

    refactor + x

--- hashcrypto.py
+++ hashcrypto.py
@@ -10,9 +10,38 @@ except ImportError:
     def b_xor(a, b):
         return bytes(map(xor, a, b))
 
+try:
+    unicode()
+except NameError:
+    unicode=str
+
+
+__version__="0.1"
+
 
 Q = Struct("<Q")
+B = Struct("<B")
 
+def pack_pascal(b):
+    if isinstance(b,unicode):
+        b=b.encode("ascii")
+    return B.pack(len(b))+b
+
+def unpack_pascal(b):
+    size=B.unpack(b[0])[0]
+    return b[1:size+1]
+
+def read_pascal(f):
+    size=B.unpack(f.read(1))[0]
+    return f.read(size)
+
+class IVError(ValueError):
+    pass
+
+
+    
+    
+    
 
 def read_file(infile, block_size):
     b = infile.read(block_size)
@@ -31,7 +60,8 @@ class HashCrypt(object):
     def block(self, iv):
         return self.hash(self.key + iv).digest()
 
-    def encrypt_file(self, infile, outfile, *args, **kwargs):
+    def encrypt_file(self, infile, outfile, write_header=False, *args, **kwargs ):
+        outfile.write(self.header())
         for block in self.encrypt(read_file(infile, self.block_size), *args, **kwargs):
             outfile.write(block)
 
@@ -39,10 +69,32 @@ class HashCrypt(object):
         for block in self.decrypt(read_file(infile, self.block_size), *args, **kwargs):
             outfile.write(block)
 
+    def header(self):
+        return b"HASHCRYPT"+(b"".join(map(pack_pascal,(__version__,self.__class__.__name__,self.hash_name))))
+    
+    def hash_name(self):
+        return self.hash().name
+    
     @classmethod
     def suggest_key_size(cls, hash_constructor=hashlib.sha512):
         return hash_constructor().block_size
 
+
+class WithIV(HashCrypt):
+    def __init__(self, key, hash_constructor=hashlib.sha512, start_iv=None):
+        super().__init__(key, hash_constructor)
+        self.start_iv=start_iv
+    def header(self):
+        return super().header()+pack_pascal(self.start_iv)
+
+    @classmethod
+    def suggest_iv_size(cls, hash_constructor=hashlib.sha512):
+        return hash_constructor().block_size
+
+    @classmethod
+    def make_iv(cls, hash_constructor=hashlib.sha512):
+        return urandom(cls.suggest_iv_size(cls, hash_constructor))
+    
 
 class CTR(HashCrypt):
 
@@ -71,6 +123,9 @@ class CTR(HashCrypt):
 
     decrypt_block = encrypt_block
 
+    def header(self):
+        return super().header()+pack_pascal(self.nonce)
+
     @classmethod
     def suggest_nonce_size(cls, hash_constructor=hashlib.sha512):
         return hash_constructor().block_size - Q.size
@@ -80,36 +135,51 @@ class CTR(HashCrypt):
         return urandom(cls.suggest_nonce_size(hash_constructor))
 
 
-class OFB(HashCrypt):
 
-    def keystream(self, iv):
+class OFB(WithIV):
+
+    def keystream(self, iv=None):
+        if iv is None:
+            iv=self.start_iv
+        if iv is None:
+            raise IVError("If iv is omitted or None, self.start_iv must be set.")
+        
         while True:
             iv = self.block(iv)
             return iv
 
-    def encrypt(self, plain_blocks, iv):
+    def encrypt(self, plain_blocks, iv=None):
+        if iv is None:
+            iv=self.start_iv
+        if iv is None:
+            raise IVError("If iv is omitted or None, self.start_iv must be set.")
+        
         return map(b_xor, plain_blocks, self.keystream(iv))
 
     decrypt = encrypt
 
-    @classmethod
-    def suggest_iv_size(cls, hash_constructor=hashlib.sha512):
-        return hash_constructor().block_size
-
-    @classmethod
-    def make_iv(cls, hash_constructor=hashlib.sha512):
-        return urandom(cls.suggest_iv_size(cls, hash_constructor))
 
 
-class CFB(HashCrypt):
 
-    def encrypt(self, plain_blocks, iv):
+class CFB(WithIV):
+
+    def encrypt(self, plain_blocks, iv=None):
+        if iv is None:
+            iv=self.start_iv
+        if iv is None:
+            raise IVError("If iv is omitted or None, self.start_iv must be set.")
+        
         for b in plain_blocks:
             d = self.block(iv)
             iv = b_xor(b, d)
             yield iv
 
-    def decrypt(self, cipher_blocks, iv):
+    def decrypt(self, cipher_blocks, iv=None):
+        if iv is None:
+            iv=self.start_iv
+        if iv is None:
+            raise IVError("If iv is omitted or None, self.start_iv must be set.")
+        
         for b in cipher_blocks:
             d = self.block(iv)
             yield b_xor(b, d)
@@ -119,6 +189,16 @@ class CFB(HashCrypt):
         d = self.block(iv)
         return b_xor(b, d)
 
-    @classmethod
-    def suggest_iv_size(cls, hash_constructor=hashlib.sha512):
-        return hash_constructor().block_size
+MODES={"CTR":CTR,"OFB":OFB,"CFB":CFB}
+ 
+
+def decrypt_file(infile,outfile,key):
+    if infile.read(9)!=b"HASHCRYPT":
+        raise ValueError("Bad Header")
+    ver=read_pascal(infile).decode("ascii")
+    mode=read_pascal(infile).decode("ascii")
+    h=read_pascal(infile).decode("ascii")
+    iv_nonce=read_pascal(infile)
+    obj=MODES[mode](key,hashlib.new(h).copy,iv_nonce)
+    obj.decrypt_file(infile,outfile)
+    
--- test_hashcrypto.py
+++ test_hashcrypto.py
@@ -4,13 +4,15 @@ import unittest
 import hashcrypto
 from os import urandom
 
+def ba(*args):
+    return bytes(bytearray(*args))
 
 class TestUtilFunctions(unittest.TestCase):
 
     def test_b_xor(self):
-        b = bytes(range(32))
-        self.assertEqual(bytes(32), hashcrypto.b_xor(b, b))
-        self.assertEqual(b, hashcrypto.b_xor(b, bytes(32)))
+        b = ba(range(32))
+        self.assertEqual(ba(32), ba(hashcrypto.b_xor(b, b)))
+        self.assertEqual(b, ba(hashcrypto.b_xor(b, ba(32))))
 
 if __name__ == '__main__':
     unittest.main()
