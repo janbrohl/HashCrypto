dad65aa2037a934ec5651cbebcfd3b00112fa351
Jan Brohl <janbrohl@t-online.de>
2016-04-16 15:35:51 +0200

    use travis

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import hashcrypto
+
+setup(name="HashCrypto",
+      version=hashcrypto.__version__,
+      description="Encryption/decryption via hashlib hash-functions",
+      author="Jan Brohl",
+      author_email="janbrohl@t-online.de",
+      url="https://github.com/janbrohl/HashCrypto",
+      test_suite="tests",
+      py_modules=[
+          'hashcrypto'
+      ],
+      scripts=["hashenc.py", "hashdec.py"],
+      classifiers=[
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.2",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+      ]
+      )
